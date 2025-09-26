348c40fbe61746fe7543529a89d04fcf469c591d
Nika Kurafeeva <nikurafeeva@gmail.com>
2025-09-26 03:37:31 +0300

    Add function task_1()

---
+++ home_work/2_hw.py
@@ -0,0 +1,14 @@
+def task_1() -> None:
+    var_int: int = 1
+    var_float: float = 1.0
+    var_str: str = "one"
+    var_list: list = [1, 1.0, "one"]
+    var_bool: bool = True
+
+    print(type(var_int))
+    print(type(var_float))
+    print(type(var_str))
+    print(type(var_list))
+    print(type(var_bool))
+
+task_1()
