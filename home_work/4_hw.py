b6c1ac02606c91b4087920c4e717752152ad8250
Nika Kurafeeva <nikurafeeva@gmail.com>
2025-10-06 12:59:22 +0300

    Add Buttons class and logic for third subtask

--- home_work/4_hw.py
+++ home_work/4_hw.py
@@ -28,6 +28,16 @@ class Math:
         return self.a / self.b
 
 
+class Button:
+    def __init__(self, text):
+        self.text = text
+        self.type = "Кнопка"
+        self.locator = ""
+
+    def click(self):
+        return f"Клик по кнопке {self.text}"
+
+
 rect1 = Rectangle(100, 100)
 rect2 = Rectangle(50, 50)
 rect3 = Rectangle(25, 25)
@@ -42,3 +52,23 @@ print(f"The addition of the values equals to {Math.addition(nums)}")
 print(f"The subtraction of the values equals to {Math.subtraction(nums)}")
 print(f"The multiplication of the values equals to {Math.multiplication(nums)}")
 print(f"The division of the values equals to {Math.division(nums)}")
+
+buttons = [
+    Button("Text Box"),
+    Button("Check Box"),
+    Button("Radio Button"),
+    Button("Web Tables"),
+    Button("Buttons"),
+    Button("Links"),
+    Button("Broken Links - Images"),
+    Button("Upload and Download"),
+    Button("Dynamic Properties")
+]
+
+print()
+print("Названия кнопок:")
+for b in buttons:
+    print(f" {b.text}")
+
+for b in buttons:
+    print(b.click())
