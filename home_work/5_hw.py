26fdc38ada74025e445a796df6a3bab342968204
Nika Kurafeeva <nikurafeeva@gmail.com>
2025-10-07 17:28:19 +0300

    Move logic into function

--- home_work/5_hw.py
+++ home_work/5_hw.py
@@ -1,25 +1,36 @@
 from selenium.webdriver.common.by import By
 from selenium  import webdriver
 
-driver = webdriver.Chrome()
-driver.get("https://www.saucedemo.com/")
+def check_login(s: str) -> bool:
+    driver = webdriver.Chrome()
+    driver.get(s)
 
-username_elem = driver.find_element(By.ID, "user-name")
-is_un = False
-if username_elem is not None:
-    is_un = True
+    is_all_elems_found = False
 
-password_elem = driver.find_element(By.ID, "password")
-is_pass = False
-if password_elem is not None:
-    is_pass = True
+    username_elem = driver.find_element(By.ID, "user-name")
+    is_un = False
+    if username_elem is not None:
+        is_un = True
 
-login_button = driver.find_element(By.ID, "login-button")
-is_login = False
-if login_button is not None:
-    is_login = True
+    password_elem = driver.find_element(By.ID, "password")
+    is_pass = False
+    if password_elem is not None:
+        is_pass = True
 
-if is_un & is_pass & is_login:
+    login_button = driver.find_element(By.ID, "login-button")
+    is_login = False
+    if login_button is not None:
+        is_login = True
+
+    if is_un & is_pass & is_login:
+        is_all_elems_found = True
+
+    return is_all_elems_found
+
+
+site = "https://www.saucedemo.com/"
+
+if check_login(site):
     print("Элементы найдены")
 else:
     print("Error")
